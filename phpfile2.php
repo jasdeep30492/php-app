60a6c6b93401df038268f032966a94d6ae7cc2e7
LoveMalhi <malhi.love98@gmail.com>
2015-12-17 21:31:35 +0530

    Create phpfile2.php

---
+++ phpfile2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello!<br>";
+echo "This is second file!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+?> 
+
+</body>
+</html>
